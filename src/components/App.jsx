92065be96cb7d9e896615041b609b7fd743a22b9
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-04 22:29:35 +0300

    add layout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,21 @@
-import { useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { FormContainer } from './App.styled';
+import { Layout } from './Layout/Layout';
 
 export function App() {
-  const [filter, setFilter] = useState('');
-
-  const onFilterChange = evt => {
-    setFilter(evt.currentTarget.value);
-  };
-
   // const visibleContacts = contacts.filter(contact =>
   //   contact.name.toLowerCase().includes(filter.toLowerCase())
   // );
 
   return (
-    <FormContainer>
+    <Layout>
       <h1>Phonebook</h1>
       <ContactForm />
 
       <h2>Contacts</h2>
-      <Filter filter={filter} onChange={onFilterChange} />
+      <Filter />
       <ContactList />
-    </FormContainer>
+    </Layout>
   );
 }
